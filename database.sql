e0b885452005b313d45d89224ce5ad7464727c5a
Fabiola Contreras <98127191+Fabiola-cc@users.noreply.github.com>
2024-04-09 16:55:22 -0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -22,3 +22,43 @@ CREATE TABLE Energía (
     max_hp DOUBLE PRECISION,
     capacitor VARCHAR
 );
+
+-- Tabla Clientes
+CREATE TABLE Clientes (
+    id_cliente INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    direccion VARCHAR(100),
+    telefono VARCHAR(8),
+    nit VARCHAR(10)
+);
+
+-- Tabla Tipos_estados
+CREATE TABLE Tipos_estados (
+    id_estado INT PRIMARY KEY,
+    nombre VARCHAR(50)
+);
+
+-- Tabla Pedidos
+CREATE TABLE Pedidos (
+    id_pedido INT PRIMARY KEY,
+    monto_total DOUBLE PRECISION,
+    estatus INT,
+    FOREIGN KEY (estatus) REFERENCES Tipos_estados(id_estado)
+);
+
+-- Tabla Factura
+CREATE TABLE Factura (
+    id_cliente INT,
+    id_pedido INT,
+    nit_empresa VARCHAR(10),
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
+    FOREIGN KEY (id_pedido) REFERENCES Pedidos(id_pedido)
+);
+
+-- Tabla Servicio
+CREATE TABLE Servicio (
+    id_servicio INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    descripcion TEXT
+);
+
