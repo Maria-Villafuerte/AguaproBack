bfefd524b11c25204f5b778099cfee633d52819d
Fabiola Contreras <fcontrerascol@gmail.com>
2024-07-18 17:36:56 -0600

    feat: get and delete endpoints

--- src/main.js
+++ src/main.js
@@ -22,19 +22,45 @@ app.get('/', (req, res) => {
 });
 
 app.get('/productos', async (req, res) => {
-    try {
-      const posts = await getProductos()
-      if (posts !== 'No posts found.') {
-        res
-          .status(200)
-          .json({ status: 'success', message: 'Posts retrieved successfully.', data: posts })
-      } else {
-        res.status(404).json({ status: 'failed', message: 'No posts found.' })
-      }
-    } catch (error) {
-      res.status(500).json({ status: 'failed', error: error.message })
+  try {
+    const posts = await getProductos()
+    if (posts !== 'No posts found.') {
+      res
+        .status(200)
+        .json({ status: 'success', message: 'Posts retrieved successfully.', data: posts })
+    } else {
+      res.status(404).json({ status: 'failed', message: 'No posts found.' })
     }
-  })
+   } catch (error) {
+    res.status(500).json({ status: 'failed', error: error.message })
+   }
+})
+
+app.get('/productos/:productId', async (req, res) => {
+  const productId = parseInt(req.params.productId, 10);
+  try {
+    const product = await getProductById(productId);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    return res.status(200).json(product);
+  } catch (error) {
+    console.error('Error fetching product by ID:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+// Eliminar un producto
+app.delete('/productos/:productId', async (req, res) => {
+  const productId = parseInt(req.params.productId, 10);
+  try {
+    await deletePost(productId);
+    res.sendStatus(204);
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`API escuchando en http://localhost:${port}`);
