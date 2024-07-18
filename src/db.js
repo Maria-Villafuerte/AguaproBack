4ddab07df5d6824191685d103489ba0faf3de207
Fabiola Contreras <fcontrerascol@gmail.com>
2024-07-18 17:35:41 -0600

    feat: get and delete functions

--- src/db.js
+++ src/db.js
@@ -5,3 +5,13 @@ export async function getProductos () {
   return result.rows.length > 0 ? result.rows : 'No posts found.'
 }
 
+// Obtener productos por ID
+export async function getProductById(productId) {
+  const [rows] = await conn.query('SELECT * FROM Productos WHERE id_producto = ?', [productId]);
+  return rows[0] || null; // Retorna el primer producto encontrado o null si no se encuentra ningún post
+}
+
+// Eliminar producto
+export async function deleteProduct(productId) {
+  await conn.query('DELETE FROM Productos WHERE id_producto = ?', [productId])
+}
