6b81aed3e42367dbeef5a67834ce640d9d069ce9
Fabiola Contreras <fcontrerascol@gmail.com>
2024-09-26 14:23:20 -0600

    chore: reorganize files - products

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
 
-// PRODUCTOS db_products
-import { getProductos, getProductById, deleteProduct, updateProduct, createProduct} from './dbFunctions/db_products.js';
 // CLIENTES db_cliente
 import { saveCliente, getAllClientes, getOneCliente, editOneCliente, deleteOneCliente } from './dbFunctions/db_cliente.js';
 // PEDIDOS db_pedidos
@@ -31,84 +29,9 @@ app.get('/', (req, res) => {
 import charsRoutes from './routes/characteristics.js';
 app.use('', charsRoutes);
 
-app.get('/productos', async (req, res) => {
-  try {
-    const posts = await getProductos()
-    if (posts !== 'No posts found.') {
-      res
-        .status(200)
-        .json({ status: 'success', message: 'Posts retrieved successfully.', data: posts })
-    } else {
-      res.status(404).json({ status: 'failed', message: 'No posts found.' })
-    }
-   } catch (error) {
-    res.status(500).json({ status: 'failed', error: error.message })
-   }
-});
-
-// Obtener información de producto individual
-app.get('/productos/:productId', async (req, res) => {
-  const productId = parseInt(req.params.productId, 10);
-  console.log(productId)
-  try {
-    const product = await getProductById(productId);
-    if (!product) {
-      return res.status(404).json({status: 'failed', error: 'Product not found' });
-    }
-    return res.status(200).json({ status: 'success', message: 'Posts retrieved successfully.', data: product });
-  } catch (error) {
-    console.error('Error fetching product by ID:', error);
-    return res.status(500).json({status: 'failed',  error: 'Internal Server Error' });
-  }
-});
-
-// Ocultar un producto
-app.put('/productos/hide/:productId', async (req, res) => {
-  const productId = parseInt(req.params.productId, 10);
-  try {
-    await deleteProduct(productId);
-    res.sendStatus(204);
-  } catch (error) {
-    console.error('Error deleting product:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-// Crear un nuevo producto
-app.post('/productos', async (req, res) => {
-  const newProduct = req.body;
-  try {
-    const createdProduct = await createProduct(newProduct);
-    return res.status(201).json({
-      status: 'success',
-      message: 'Product created successfully.',
-      data: createdProduct
-    });
-  } catch (error) {
-    console.error('Error creating product:', error);
-    return res.status(500).json({
-      status: 'failed',
-      error: 'Internal Server Error'
-    });
-  }
-});
-
-// Endpoint para actualizar un producto
-app.put('/productos/:productId', async (req, res) => {
-  const productId = parseInt(req.params.productId, 10);
-  const { nombre, descripción, tipo_producto } = req.body;
-
-  try {
-    const updated = await updateProduct(productId, nombre, descripción, tipo_producto);
-    if (!updated) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-    res.status(200).json({ message: 'Product updated successfully' });
-  } catch (error) {
-    console.error('Error updating product:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// Usar las rutas de productos
+import productsRoutes from './routes/products.js';
+app.use('', productsRoutes);
 
 app.post('/register', async (req, res) => {
   const { username, password, email, role } = req.body;
