687ec8638ee7b2d483b446f13c235420f9806143
Fabiola Contreras <fcontrerascol@gmail.com>
2024-10-14 10:10:21 -0600

    add: mail sending logic

--- src/main.js
+++ src/main.js
@@ -38,6 +38,10 @@ app.use('', pedidosRoutes);
 import statisticsRoutes from './routes/statistics.js';
 app.use('', statisticsRoutes);
 
+// Usar la ruta de mail
+import mailRoute from './routes/mail.js';
+app.use('', mailRoute);
+
 // Exportamos app para poder usarla en los tests
 export default app;
 
