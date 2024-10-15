83558060a31634672084d4e46711cb02f9f87652
Fabiola Contreras <fcontrerascol@gmail.com>
2024-10-15 10:11:45 -0600

    add: new mail routes

---
+++ __tests__/mail/solicitudPago.test.js
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import app from '../../src/main.js';
+
+let server;
+
+beforeAll(() => {
+  server = app.listen(0);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+describe('Mailer API tests for required payment', () => {
+    it('should send an email confirmation', async () => {
+        const response = await request(app)
+            .post('/pedido/pago')
+            .send({ mailto: 'con22787@uvg.edu.gt'}); 
+
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('Correo enviado');
+    });
+
+    it('should return 400 for invalid email', async () => {
+        const response = await request(app)
+            .post('/pedido/pago')
+            .send({ mailto: 'invalid-email' });
+
+        expect(response.status).toBe(400);
+        expect(response.text).toBe('Correo electrónico inválido');
+    });
+});
--- src/routes/mail.js
+++ src/routes/mail.js
@@ -28,8 +28,71 @@ export async function sendEmail(mailto, subject, html) {
     transporter.sendMail(mailOptions)
 }
 
-router.post('/confirmacion', async (req, res) => {
-    const { mailto, subject, html } = req.body;
+router.post('/confirmacion/pedido', async (req, res) => {
+    const { mailto } = req.body;
+
+    let subject = 'Gracias por tu compra en Aguatesa en línea'
+    let html = '<h1>Gracias por tu compra</h1>'
+
+    // Validar el correo electrónico
+    if (!mailto || !/\S+@\S+\.\S+/.test(mailto)) {
+        return res.status(400).send('Correo electrónico inválido');
+    }
+
+    try {
+        await sendEmail(mailto, subject, html);
+        res.status(200).send('Correo enviado');
+    } catch (error) {
+        console.error(error); // Log del error en consola
+        res.status(500).send('Error al enviar el correo');
+    }
+});
+
+router.post('/confirmacion/servicio', async (req, res) => {
+    const { mailto } = req.body;
+
+    let subject = 'Recibimos tu solicitud de servicio'
+    let html = '<h1>Gracias por conectar con nosotros, pronto te estaremos contactando</h1>'
+
+    // Validar el correo electrónico
+    if (!mailto || !/\S+@\S+\.\S+/.test(mailto)) {
+        return res.status(400).send('Correo electrónico inválido');
+    }
+
+    try {
+        await sendEmail(mailto, subject, html);
+        res.status(200).send('Correo enviado');
+    } catch (error) {
+        console.error(error); // Log del error en consola
+        res.status(500).send('Error al enviar el correo');
+    }
+});
+
+router.post('/cancelacion', async (req, res) => {
+    const { mailto } = req.body;
+
+    let subject = 'Hemos cancelado tu pedido en Aguatesa'
+    let html = '<h1>Por estas razones tu pedido fue cancelado, comunicate con nosotros</h1>'
+
+    // Validar el correo electrónico
+    if (!mailto || !/\S+@\S+\.\S+/.test(mailto)) {
+        return res.status(400).send('Correo electrónico inválido');
+    }
+
+    try {
+        await sendEmail(mailto, subject, html);
+        res.status(200).send('Correo enviado');
+    } catch (error) {
+        console.error(error); // Log del error en consola
+        res.status(500).send('Error al enviar el correo');
+    }
+});
+
+router.post('/pedido/pago', async (req, res) => {
+    const { mailto } = req.body;
+
+    let subject = 'Gracias por tu compra en Aguatesa en línea'
+    let html = '<h1>Hemos recibido tu pedido, te enviamos los datos para que puedas realiar tu pago</h1>'
 
     // Validar el correo electrónico
     if (!mailto || !/\S+@\S+\.\S+/.test(mailto)) {
