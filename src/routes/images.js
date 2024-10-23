dd11f9dba6e23ad431cc76ab505852bb730c988f
Fabiola Contreras <fcontrerascol@gmail.com>
2024-10-23 12:58:54 -0600

    fix: image upload process

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,8 +1,6 @@
 import { google } from 'googleapis';
 import express from 'express';
-import multer from 'multer'; // Para manejar el archivo subido
-import fs from 'fs';
-import path from 'path'; // Para manejar las rutas de archivos
+import multer from 'multer';
 
 const router = express.Router();
 
@@ -20,44 +18,33 @@ oauth2Client.setCredentials({
 // Inicializa el servicio de Google Drive
 const drive = google.drive({
   version: 'v3',
-  auth: oauth2Client
+  auth: oauth2Client,
 });
 
-// Configuración de multer para manejar archivos subidos
-const upload = multer({ dest: 'uploads/' });
+// Configuración de multer para manejar archivos en memoria
+const upload = multer({ storage: multer.memoryStorage() });
 
 // POST para subir imágenes a Google Drive
 router.post('/upload', upload.single('file'), async (req, res) => {
   try {
-    // Verificar que el archivo exista
-    if (!req.file) {
-      return res.status(400).send('No se ha proporcionado un archivo.');
-    }
-
-    // Metadatos del archivo
     const fileMetadata = {
       name: req.file.originalname, // Nombre del archivo en Google Drive
     };
 
-    // Media object para Google Drive
     const media = {
       mimeType: req.file.mimetype,
-      body: fs.createReadStream(path.join(__dirname, '..', req.file.path)), // Lee el archivo subido como flujo
+      body: Buffer.from(req.file.buffer), // Utiliza el buffer directamente
     };
 
-    // Subir el archivo a Google Drive
     const response = await drive.files.create({
       resource: fileMetadata,
       media: media,
       fields: 'id',
     });
 
-    // Elimina el archivo temporal después de subirlo
-    fs.unlinkSync(req.file.path);
-
     res.status(200).json({ fileId: response.data.id });
   } catch (error) {
-    console.error('Error al subir el archivo:', error);
+    console.error(error);
     res.status(500).json({ error: 'Error al subir el archivo' });
   }
 });
