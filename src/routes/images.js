98e9e9d9b35b24126a5562145fff4adec3c4a7d6
Fabiola Contreras <fcontrerascol@gmail.com>
2024-10-23 13:12:22 -0600

    add: access token

--- src/routes/images.js
+++ src/routes/images.js
@@ -13,6 +13,7 @@ const oauth2Client = new google.auth.OAuth2(
 );
 
 oauth2Client.setCredentials({
+  access_token: process.env.GOOGLE_ACCESS_TOKEN,
   refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
 });
 
