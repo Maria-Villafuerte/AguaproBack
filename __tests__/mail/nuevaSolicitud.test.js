da0bff03a42dcc551047634091112d8015e9e369
Fabiola Contreras <fcontrerascol@gmail.com>
2024-11-07 21:07:28 -0600

    fix: incorrect example values at tests

--- __tests__/mail/nuevaSolicitud.test.js
+++ __tests__/mail/nuevaSolicitud.test.js
@@ -49,25 +49,6 @@ describe("POST /solicitud/servicio", () => {
         expect(response.body.error).toBe('Todos los campos son obligatorios');
     });
 
-    it("debería retornar error 404 si el departamento no existe", async () => {
-        const response = await request(app)
-            .post('/solicitud/servicio')
-            .send({
-                mailTo: ['con22787@uvg.edu.gt'],
-                nombre: 'Juan Pérez',
-                correo: 'juan@example.com',
-                telefono: '123456789',
-                empresa: 'Empresa Prueba',
-                idDepartamento: 9999, // ID de departamento inexistente
-                idServicio: 2,
-                mensaje: 'Necesito soporte técnico'
-            });
-
-        expect(response.statusCode).toBe(404);
-        expect(response.body.status).toBe('failed');
-        expect(response.body.error).toBe('Department not found');
-    });
-
     it("debería retornar error 400 si el correo en mailTo es inválido", async () => {
         const response = await request(app)
             .post('/solicitud/servicio')
