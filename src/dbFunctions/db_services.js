22a6011156f5ce8eb05503ac82be52088bf9cd20
Fabiola Contreras <fcontrerascol@gmail.com>
2024-11-07 20:50:13 -0600

    fix: function result

--- src/dbFunctions/db_services.js
+++ src/dbFunctions/db_services.js
@@ -21,13 +21,13 @@ export async function getServicios() {
 export async function getDepartamentoById(idDepartamento) {
   const result = await conn.query(`SELECT nombre FROM Departamentos 
       WHERE id_departamento = $1`, idDepartamento)
-  return result.rows.length > 0 ? result.rows : 'No requests found.'
+  return result.rows.length > 0 ? result.rows[0] : 'No requests found.'
 }
 
 export async function getServicioById(idServicio) {
   const result = await conn.query(`SELECT nombre FROM Servicios 
       WHERE id_tipo = $1`, idServicio)
-  return result.rows.length > 0 ? result.rows : 'No requests found.'
+  return result.rows.length > 0 ? result.rows[0] : 'No requests found.'
 }
 
 // Crear solicitud
